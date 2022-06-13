76656393e10f27cce021847690fe33ede979f50a
AshwinLechnerRea <107248882+AshwinLechnerRea@users.noreply.github.com>
2022-06-13 14:44:54 +0200

    Changed function name opdracht3

--- Reguliere-expressies/Opdracht3/js/script.js
+++ Reguliere-expressies/Opdracht3/js/script.js
@@ -1,10 +1,10 @@
 let users = "Branford, Terra\nCole, Locke\n Garamonde, Cyan";
 let users2 = "Branford, Terra\nCole, Locke\nGaramonde, Cyan\nChère, Celes";
 
-function replace(match, $1, $2) {
+function toUpper(match, $1, $2) {
     return $2 + " " + $1.toUpperCase();
 }
 
 console.log(users.replace(/([\w ]+), ([\w ]+)/g, "$2 $1"));
 console.log(users2.replace(/([A-zÀ-ú]+), ([A-zÀ-ú]+)/g, "$2 $1"));
-console.log(users2.replace(/([A-zÀ-ú]+), ([A-zÀ-ú]+)/g, replace));
+console.log(users2.replace(/([A-zÀ-ú]+), ([A-zÀ-ú]+)/g, toUpper));
