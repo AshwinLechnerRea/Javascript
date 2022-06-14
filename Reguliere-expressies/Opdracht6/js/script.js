d4cf81fb88aa71089371281329202f275d3bf527
AshwinLechnerRea <107248882+AshwinLechnerRea@users.noreply.github.com>
2022-06-14 13:03:36 +0200

    Added opdracht6

---
+++ Reguliere-expressies/Opdracht6/js/script.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+    let phonePattern = /^(06|06[\s]|06-|\+31[\s]6[\s])[0-9]{8}$/;
+    let birthdayPattern = /^([\d]{1,2})(-|[\s])([a-z]|[0-9]{2,})/;
+    let streetPattern = /^([a-z]*)([\s]+)([0-9]{1,3})(((-|[\s])?)([a-z]?))$/i;
+    let urlPattern = /^(http)s?:\/\//i;
+    let passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])/;
+
+    $("#submit-number").click(function (e) {
+        e.preventDefault();
+        let phoneNumber = $("#phone-number").val();
+        let birthday = $("#birthday").val();
+        let street = $("#street").val();
+        let url = $("#url").val();
+        let password = $("#password").val();
+        try {
+            if (phonePattern.test(phoneNumber) == false) {
+                $(".phone-error").show();
+                throw "Invalid phone number!";
+            }
+        } catch (err) {
+            console.warn(err);
+        }
+        try {
+            if (birthdayPattern.test(birthday) == false) {
+                $(".birthday-error").show();
+                throw "Invalid birthday!";
+            }
+        } catch (err) {
+            console.warn(err);
+        }
+        try {
+            if (streetPattern.test(street) == false) {
+                $(".street-error").show();
+                throw "Invalid street!";
+            }
+        } catch (err) {
+            console.warn(err);
+        }
+        try {
+            if (urlPattern.test(url) == false) {
+                $(".url-error").show();
+                throw "Invalid street!";
+            }
+        } catch (err) {
+            console.warn(err);
+        }
+        try {
+            if (passwordPattern.test(password) == false) {
+                $(".password-error").show();
+                throw "Invalid password!";
+            }
+        } catch (err) {
+            console.warn(err);
+        }
+    })
+});
