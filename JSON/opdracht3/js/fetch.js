7af3e74fcf68ac89740bd85cdb1858ff444c60ab
AshwinLechnerRea <107248882+AshwinLechnerRea@users.noreply.github.com>
2022-07-07 14:04:02 +0200

    Added opdracht3 JSON

--- JSON/opdracht3/js/fetch.js
+++ JSON/opdracht3/js/fetch.js
@@ -2,7 +2,20 @@ fetch("electiondata.php")
     .then(response => response.json())
     .then(data => {
         let html = ""
+        let table = document.createElement("table");
+        html += `  
+            <tr>
+                 <td>Partij</td>
+                 <td>Zetels</td>
+            </tr>
+        `
         for (const party in data) {
-            console.log(party, zetels)
+            html += `
+             <tr>
+                 <td>${party}</td>
+                 <td>${data[party]}</td>
+            </tr>`
         }
+        table.innerHTML = html;
+        document.querySelector("body").appendChild(table);
     });
